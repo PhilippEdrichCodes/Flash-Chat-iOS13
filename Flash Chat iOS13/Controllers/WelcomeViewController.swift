1c4167ba7e847653c60a9ad547e93a4ec95ddd94
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-29 22:07:44 +0100

    add nav controller

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        printAppLogo()
     }
     
-
+    func printAppLogo() {
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+        Timer.scheduledTimer(withTimeInterval: charIndex * 0.1, repeats: false) {
+            (timer) in
+           
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
 }
